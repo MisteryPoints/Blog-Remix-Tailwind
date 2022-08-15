9728cb4abc59fa6cf179c9f2daa24dcc0f1a5f3e
Víctor Tejada <105092810+MisteryPoints@users.noreply.github.com>
2022-08-15 17:15:46 +0000

    deploy

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,15 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build",
+  serverBuildPath: "build/index.js",
+  publicPath: "/build/",
+  serverDependenciesToBundle: [
+    /^rehype.*/,
+    /^remark.*/,
+    /^unified.*/,
+    "@sindresorhus/slugify",
+    "marked"
+  ],
 };
