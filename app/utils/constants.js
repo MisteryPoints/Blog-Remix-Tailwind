1228e3534ba6c3d72d845c7bd3f086f52433b1b3
Víctor Tejada <105092810+MisteryPoints@users.noreply.github.com>
2022-08-15 16:59:37 +0000

    deploy

--- app/utils/constants.js
+++ app/utils/constants.js
@@ -1,3 +1,3 @@
 // export const BASE_PATH = 'http://10.0.0.2:3000'
-export const BASE_PATH = 'https://misterypoints-blog-remix-tailwind-974r9x4pjfqw7-3000.githubpreview.dev'
-// export const BASE_PATH = 'https://blog-remix-devpoint.netlify.app'
+// export const BASE_PATH = 'https://misterypoints-blog-remix-tailwind-974r9x4pjfqw7-3000.githubpreview.dev'
+export const BASE_PATH = 'https://blog-remix-devpoint.netlify.app'
